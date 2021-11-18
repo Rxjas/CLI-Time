4a35fea2e889ce94407fb0b46dfdbcc90e680144
rxjas <christopherrojas35@yahoo.com>
2021-11-18 00:05:24 -0700

    added connecting the roots to the tree

--- CLI-Time.js
+++ CLI-Time.js
@@ -1,1 +1,12 @@
 //This file will bring all the apps from the apps folder to life
+const appTreeRoot = require('./apptree');
+const inquirer = require('inquirer');
+startTree();
+
+function startTree(){
+    branchConnections();
+};
+
+function branchConnections(){
+    console.log(appTreeRoot)
+};
